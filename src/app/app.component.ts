bc80dfacbbdd647d1245eb54568e044cf58dd5d5
Akshita13 <akshitakapadia13@gmail.com>
2021-01-20 16:33:33 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,29 +9,68 @@ import { FormArray, FormBuilder, FormGroup } from "@angular/forms";
 export class AppComponent {
   myForm: FormGroup;
   submittedHours: FormArray;
+  hours: FormArray;
+
   public total = 0;
+  public total1=0;
+  public total2=0;
   constructor(private fb: FormBuilder) {}
   name = "Angular";
-  arr = [1, 2, 3, 4, 5, 6, 7];
+  arr = [1, 2, 3, 4, 5, 6, 7]
+  bsubmitHours = [0,0,0,0,0,0,0,0];
+  nbsubmitHours = [0,0,0,0,0,0,0,0];
+
   ngOnInit() {
+    
     this.myForm = this.fb.group({
       submittedHours: this.fb.array([])
     });
     this.submittedHours = this.myForm.get("submittedHours") as FormArray;
-    for (let i = 0; i < 7; i++) {
+    
+    for (let i = 0; i < 2; i++) {
       this.submittedHours.push(this.createItem());
+      console.log(this.submittedHours.controls[i].get('hours'),"jjhg")
+     this.hours = this.submittedHours.controls[i].get('hours') as FormArray;
+       for(let j=0;j<7;j++){
+      this.hours.push(this.createForm())
     }
+    }
+  
   }
 
   createItem() {
     return this.fb.group({
-      hours: [""]
+      hours: this.fb.array([])
     });
   }
 
-  addHours(type, value, index, control, formGroup) {
-    // debugger;
-    this.total = this.total + parseInt(value)
-    console.log(this.total, "total");
+  createForm(){
+   return this.fb.group({
+      submittedHours: ['']
+    });
+
+  }
+
+  addHours(type, value, index, formGroup) {
+    debugger;
+    console.log(this.hours)
+    if(type=='billable'){
+      this.bsubmitHours[index]=parseInt(value)
+      this.total1=this.total1+this.bsubmitHours[index]
+     console.log(this.total1, this.bsubmitHours,"total1")
+    // this.total1 = this.total1 + parseInt(value)
+    }else if(type=='nonbillable'){
+
+      this.nbsubmitHours[index]=parseInt(value)
+      this.total2=this.total2+this.nbsubmitHours[index]
+      // this.total2=this.total2 + parseInt(value)
+    }
+//  console.log(this.submitHours,"this.submitHours[index][0]")
+    this.total=this.total1+this.total2
+    console.log(this.total1,this.total2, "total");
+  }
+
+  onSubmit(id){
+    debugger
   }
 }
